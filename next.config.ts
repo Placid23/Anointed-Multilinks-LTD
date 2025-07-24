ec770e00bc2b6d7c343f254b4196a17cefceebf6
Kingsley Placid <pckingsley27@gmail.com>
2025-07-24 10:44:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -18,6 +19,13 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_PROCESSING_APP_URL: process.env.NEXT_PUBLIC_PROCESSING_APP_URL,
+    PROCESSING_APP_API_KEY: process.env.PROCESSING_APP_API_KEY,
+  }
 };
 
 export default nextConfig;
